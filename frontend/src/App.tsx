125270d1ef04b642ca2284b8c180b415ec3f8750
Emerson <emerson_c.morais@hotmail.com>
2021-05-03 20:55:26 -0300

    feat: create static charts components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,35 @@
+import { BarChart } from "components/Barchart/idex";
 import { DataTable } from "components/DataTable";
+import { DonutChart } from "components/DonutChart";
 import { Footer } from "components/Footer";
 import { Navbar } from "components/Navbar";
 export function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="container">
-        <h1>Ola Mundo</h1>
-        <DataTable/>
-      </div>
-      <Footer/>
-    </>
-  )}
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">
+              Resultado de Vendas por colaborador(%)
+            </h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">
+              Resultado de Vendas por colaborador(%)
+            </h5>
+            <DonutChart />
+          </div>
+        </div>
 
+        <div className="py-3">
+          <h2 className="text-primary">TODAS VENDAS</h2>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
+  );
+}
