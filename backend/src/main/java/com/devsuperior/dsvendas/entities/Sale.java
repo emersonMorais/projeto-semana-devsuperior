d000c3d8ad0504428a8f83f681ac723ad8c517c3
Emerson <emerson_c.morais@hotmail.com>
2021-05-08 00:47:08 -0300

    chore: add first homolog

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -1,8 +1,5 @@
 package com.devsuperior.dsvendas.entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
