daaad703a3403a6649051ca852494d2ca7feef5f
Emerson <emerson_c.morais@hotmail.com>
2021-05-08 17:33:50 -0300

    chore: create component Routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,36 @@
+import { BarChart } from "components/Barchart/idex";
+import { DataTable } from "components/DataTable";
+import { DonutChart } from "components/DonutChart";
+import { Footer } from "components/Footer";
+import { Navbar } from "components/Navbar";
+
+export function Dashboard() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">
+              Resultado de Vendas por colaborador(%)
+            </h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">
+              Resultado de Vendas por colaborador(%)
+            </h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">TODAS VENDAS</h2>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
+  );
+}
---
+++ frontend/src/pages/HOME/index.tsx
@@ -0,0 +1,6 @@
+export function Home() {
+  return (
+    <h1>Página Home sem estilos</h1>
+  )
+}
+
