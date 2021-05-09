dbcc1a409e698616ea74d3ab4580e86bbd3c59ad
Emerson <emerson_c.morais@hotmail.com>
2021-05-08 22:51:45 -0300

    chore: add DataTable intregation

--- frontend/src/components/Barchart/idex.tsx
+++ frontend/src/components/Barchart/idex.tsx
@@ -1,6 +1,56 @@
-import Chart from 'react-apexcharts';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import { SaleSuccess } from "types/sale";
+import { round } from "utils/format";
+import { BASE_URL } from "utils/requests";
 
+type SeriesData = {
+  name: string;
+  data: number[];
+};
+
+type ChartData = {
+  labels: {
+    categories: string[];
+  };
+  series: SeriesData[];
+};
 export function BarChart() {
+  const [chartaData, setChartData] = useState<ChartData>({
+    labels: {
+      categories: [],
+    },
+    series: [
+      {
+        name: "",
+        data: [],
+      },
+    ],
+  });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`).then((response) => {
+      const data = response.data as SaleSuccess[];
+      const myLabels = data.map((label) => label.sellerName);
+      const mySeries = data.map((serie) =>
+        round(100 * (serie.deals / serie.visited), 1)
+      );
+
+      setChartData({
+        labels: {
+          categories: myLabels,
+        },
+        series: [
+          {
+            name: "% Success",
+            data: mySeries,
+          },
+        ],
+      });
+    });
+  }, []);
+
   const options = {
     plotOptions: {
       bar: {
@@ -9,24 +59,12 @@ export function BarChart() {
     },
   };
 
-  const mockData = {
-    labels: {
-      categories: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-    },
-    series: [
-      {
-        name: "% Sucesso",
-        data: [43.6, 67.1, 67.7, 45.6, 71.1],
-      },
-    ],
-  };
-
   return (
-    <Chart 
-      options={{...options, xaxis: mockData.labels}}
-      series={mockData.series}
+    <Chart
+      options={{ ...options, xaxis: chartaData.labels }}
+      series={chartaData.series}
       type="bar"
       height="240"
     />
-  )
+  );
 }
